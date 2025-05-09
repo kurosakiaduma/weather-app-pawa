7238b84947344df7fb41b95c3838eca1d3144e56
Tevin Aduma <tevin74@live.com>
2025-05-10 02:24:16 +0300

    feat: add wind direction to WeatherData interface

--- frontend/app/interfaces/WeatherData.ts
+++ frontend/app/interfaces/WeatherData.ts
@@ -2,27 +2,27 @@
  * Interface for Weather data from backend API
  */
 export interface WeatherData {
-    current: {
-      temp: number;
-      humidity: number;
-      wind_speed: number;
-      weather: {
-        description: string;
-        icon: string;
-      }[];
-      dt: number; // timestamp
-    };
-    daily: {
-      dt: number; // timestamp
-      temp: {
-        day: number;
-      };
-      weather: {
-        description: string;
-        icon: string;
-      }[];
+  current: {
+    temp: number;
+    humidity: number;
+    wind_speed: number;
+    wind_deg: number;
+    weather: {
+      description: string;
+      icon: string;
     }[];
-    city: string;
-    country: string;
-  }
-  
+    dt: number; // timestamp
+  };
+  daily: {
+    dt: number; // timestamp
+    temp: {
+      day: number;
+    };
+    weather: {
+      description: string;
+      icon: string;
+    }[];
+  }[];
+  city: string;
+  country: string;
+}
