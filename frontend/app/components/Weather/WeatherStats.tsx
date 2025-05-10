10b5d20e09f48b0174e6b580048e91be48b14207
Tevin Aduma <tevin74@live.com>
2025-05-10 03:25:01 +0300

    style: remove fixed height from WeatherStats icon for better responsiveness

--- frontend/app/components/Weather/WeatherStats.tsx
+++ frontend/app/components/Weather/WeatherStats.tsx
@@ -36,7 +36,7 @@ const WeatherStats: React.FC<WeatherStatsProps> = ({ windSpeed, humidity, windDe
             >
               <svg 
                 xmlns="http://www.w3.org/2000/svg" 
-                className="h-5 w-5 text-white" 
+                className="text-white" 
                 fill="none" 
                 viewBox="0 0 24 24" 
                 stroke="currentColor"
